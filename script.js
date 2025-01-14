2b63a87cf4a6d76e4db783e83108f9ed036894bd
YourGitHubUsername <khadsemayank@gmail.com>
2025-01-14 22:34:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showWish() {
+    var surprise = document.getElementById("surprise");
+    surprise.classList.remove("hidden");
+    document.getElementById("message").innerText = "Hope you have a fantastic birthday!";
+}
